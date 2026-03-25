23871466f9a357d4108315ab87dbb8afb4af9f87
TuanAn <pantoan288@gmail.com>
2026-03-25 15:44:19 +0700

    refactor: streamline dark mode handling in App.jsx and remove adundant from others

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import Router from "./router/Router";
 import publicRoutes from "./router/routes/publicRoutes";
 import { getRoutes } from "./router/routes";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { get_user_info } from "./store/Reducers/authReducer";
 
 function App() {
   const dispatch = useDispatch()
+  const darkMode = useSelector((state) => state.theme.darkMode)
   const [allRoutes, setAllRoutes] = useState([...publicRoutes])
   const [authReady, setAuthReady] = useState(false)
+
+  useLayoutEffect(() => {
+    document.documentElement.classList.toggle('dark', darkMode)
+  }, [darkMode])
 
   useEffect(() => {
     const routes = getRoutes()
--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -57,7 +57,6 @@ const AdminLogin = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { loader, errorMessage, successMessage, role } = useSelector((s) => s.auth);
-    const { darkMode } = useSelector((s) => s.theme);
     const [state, setState] = useState({ email: '', password: '' });
 
     const inputHandler = (e) => setState({ ...state, [e.target.name]: e.target.value });
@@ -74,8 +73,8 @@ const AdminLogin = () => {
     }, [role, navigate]);
 
     return (
-        <div className={`min-h-screen font-montserrat bg-surface dark:bg-dark transition-colors duration-200
-            flex flex-col justify-center items-center px-4 py-12 ${darkMode ? 'dark' : ''}`}>
+        <div className="min-h-screen font-montserrat bg-surface dark:bg-dark transition-colors duration-200
+            flex flex-col justify-center items-center px-4 py-12">
 
             {/* Brand wordmark */}
             <div className="flex items-center gap-2 mb-8 text-accent">
